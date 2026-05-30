2acfccdea149e9e28c67b403ca59b22e022aa59c
amirtm <amirtm8080@gmail.com>
2026-05-30 17:41:42 +0100

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('cfp-v1').then(cache =>
+      cache.addAll(['/', '/index.html', '/manifest.json', '/logo.jpg'])
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request)
+    )
+  );
+});
